3297436458d65e9916107af69bc89058d9c32e5a
lutzzdias <thiagolutzdias@hotmail.com>
2025-08-16 01:11:41 -0400

    feat: add reading-time plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import { remarkReadingTime } from "./src/plugins/reading-time";
 
 export default defineConfig({
   prefetch: true,
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
   },
 });
